213e5def8cf6f98ff0d39efcbf805a5eb79ce0ea
Ha Duy Trung <haduytrung@gmail.com>
2015-01-25 16:13:43 +0800

    Close drawer on navigation

--- app/src/main/java/io/github/hidroh/materialistic/DrawerFragment.java
+++ app/src/main/java/io/github/hidroh/materialistic/DrawerFragment.java
@@ -1,8 +1,10 @@
 package io.github.hidroh.materialistic;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,6 +12,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class DrawerFragment extends Fragment {
+
+    private static final long DRAWER_SLIDE_DURATION_MS = 250;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -23,24 +27,26 @@ public class DrawerFragment extends Fragment {
         view.findViewById(R.id.drawer_settings).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final Intent intent;
                 if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-                    intent = new Intent(getActivity(), ActionBarSettingsActivity.class);
+                    navigate(ActionBarSettingsActivity.class);
                 } else {
-                    intent = new Intent(getActivity(), SettingsActivity.class);
+                    navigate(SettingsActivity.class);
                 }
-                getActivity().startActivity(intent);
             }
         });
         return view;
     }
 
-    private void navigate(Class<? extends BaseActivity> activityClass) {
-        if (getActivity().getClass().equals(activityClass)) {
-            ((BaseActivity) getActivity()).closeDrawers();
-        } else {
-            final Intent intent = new Intent(getActivity(), activityClass);
-            getActivity().startActivity(intent);
+    private void navigate(final Class<? extends Activity> activityClass) {
+        ((BaseActivity) getActivity()).closeDrawers();
+        if (!getActivity().getClass().equals(activityClass)) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    final Intent intent = new Intent(getActivity(), activityClass);
+                    getActivity().startActivity(intent);
+                }
+            }, DRAWER_SLIDE_DURATION_MS);
         }
     }
 
