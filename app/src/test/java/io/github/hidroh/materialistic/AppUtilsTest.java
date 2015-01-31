3dea11de58945b87da30ff230234736219c705c1
Ha Duy Trung <haduytrung@gmail.com>
2015-01-31 21:55:44 +0800

    Add robolectric

---
+++ app/src/test/java/io/github/hidroh/materialistic/AppUtilsTest.java
@@ -0,0 +1,22 @@
+package io.github.hidroh.materialistic;
+
+import android.content.Intent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.assertj.android.api.Assertions.assertThat;
+
+@Config(emulateSdk = 18, reportSdk = 18)
+@RunWith(RobolectricTestRunner.class)
+public class AppUtilsTest {
+    @Test
+    public void testMakeShareIntent() {
+        Intent actual = AppUtils.makeShareIntent("content");
+        assertThat(actual).hasAction(Intent.ACTION_SEND);
+        assertThat(actual).hasType("text/plain");
+        assertThat(actual).hasExtra(Intent.EXTRA_TEXT);
+    }
+}
