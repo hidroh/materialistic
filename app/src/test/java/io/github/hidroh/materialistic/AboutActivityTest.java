07551bbb36c0cb65a898611251e53560aa6e6044
Ha Duy Trung <haduytrung@gmail.com>
2015-04-19 16:39:06 +0800

    Add about activity test

---
+++ app/src/test/java/io/github/hidroh/materialistic/AboutActivityTest.java
@@ -0,0 +1,24 @@
+package io.github.hidroh.materialistic;
+
+import android.widget.TextView;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import static junit.framework.Assert.assertFalse;
+import static org.assertj.android.api.Assertions.assertThat;
+
+@RunWith(RobolectricTestRunner.class)
+public class AboutActivityTest {
+    @Test
+    public void test() {
+        AboutActivity activity = Robolectric.setupActivity(AboutActivity.class);
+        assertThat((TextView) activity.findViewById(R.id.text_application_info)).containsText("Version");
+        assertThat((TextView) activity.findViewById(R.id.text_developer_info)).containsText("Ha Duy Trung");
+        assertThat((TextView) activity.findViewById(R.id.text_libraries)).isNotEmpty();
+        assertThat((TextView) activity.findViewById(R.id.text_license)).containsText("Apache");
+        assertFalse(activity.isSearchable());
+    }
+}
