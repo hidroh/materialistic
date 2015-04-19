a6902a7bd78429cbcea1a70cb0f5035bd119e400
Ha Duy Trung <haduytrung@gmail.com>
2015-04-19 23:55:13 +0800

    Add search activity test

---
+++ app/src/test/java/io/github/hidroh/materialistic/SearchActivityTest.java
@@ -0,0 +1,55 @@
+package io.github.hidroh.materialistic;
+
+import android.app.SearchManager;
+import android.content.Intent;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.util.ActivityController;
+
+import io.github.hidroh.materialistic.test.ShadowSearchRecentSuggestions;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Config(shadows = ShadowSearchRecentSuggestions.class)
+@RunWith(RobolectricTestRunner.class)
+public class SearchActivityTest {
+    private ActivityController<SearchActivity> controller;
+    private SearchActivity activity;
+
+    @Before
+    public void setUp() {
+        ShadowSearchRecentSuggestions.recentQueries.clear();
+        controller = Robolectric.buildActivity(SearchActivity.class);
+        activity = controller.get();
+    }
+
+    @Test
+    public void testCreateWithQuery() {
+        Intent intent = new Intent();
+        intent.putExtra(SearchManager.QUERY, "filter");
+        controller.withIntent(intent).create().start().resume(); // skip menu inflation
+        assertThat(ShadowSearchRecentSuggestions.recentQueries).contains("filter");
+        assertEquals(activity.getString(R.string.title_activity_search_query, "filter"),
+                activity.getDefaultTitle());
+        assertFalse(activity.isItemOptionsMenuVisible());
+        controller.pause().stop().destroy();
+    }
+
+
+    @Test
+    public void testCreateWithoutQuery() {
+        controller.create().start().resume(); // skip menu inflation
+        assertThat(ShadowSearchRecentSuggestions.recentQueries).isEmpty();
+        assertEquals(activity.getString(R.string.title_activity_search),
+                activity.getDefaultTitle());
+        assertFalse(activity.isItemOptionsMenuVisible());
+        controller.pause().stop().destroy();
+    }
+}
--- app/src/test/java/io/github/hidroh/materialistic/test/ShadowSearchRecentSuggestions.java
+++ app/src/test/java/io/github/hidroh/materialistic/test/ShadowSearchRecentSuggestions.java
@@ -5,12 +5,21 @@ import android.provider.SearchRecentSuggestions;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Implements(value = SearchRecentSuggestions.class, inheritImplementationMethods = true)
 public class ShadowSearchRecentSuggestions {
     public static int historyClearCount = 0;
+    public static List<String> recentQueries = new ArrayList<>();
 
     @Implementation
     public void clearHistory() {
         historyClearCount++;
+    }
+
+    @Implementation
+    public void saveRecentQuery(final String queryString, final String line2) {
+        recentQueries.add(queryString);
     }
 }
