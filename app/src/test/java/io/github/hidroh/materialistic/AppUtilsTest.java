ffce654b935912dd3c6af9e8a3eb6196525bd012
Ha Duy Trung <haduytrung@gmail.com>
2015-04-19 23:33:58 +0800

    Add app utils test

--- app/src/test/java/io/github/hidroh/materialistic/AppUtilsTest.java
+++ app/src/test/java/io/github/hidroh/materialistic/AppUtilsTest.java
@@ -1,12 +1,21 @@
 package io.github.hidroh.materialistic;
 
 import android.content.Intent;
+import android.view.MotionEvent;
+import android.widget.TextView;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.shadows.ShadowApplication;
 
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
 import static org.assertj.android.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.robolectric.Shadows.shadowOf;
 
 @RunWith(RobolectricTestRunner.class)
 public class AppUtilsTest {
@@ -16,5 +25,21 @@ public class AppUtilsTest {
         assertThat(actual).hasAction(Intent.ACTION_SEND);
         assertThat(actual).hasType("text/plain");
         assertThat(actual).hasExtra(Intent.EXTRA_TEXT);
+    }
+
+    @Test
+    public void testSetTextWithLinks() {
+        TextView textView = new TextView(RuntimeEnvironment.application);
+        AppUtils.setTextWithLinks(textView, "<a href=\\\"http://www.justin.tv/problems/bml\\\" rel=\\\"nofollow\\\">http://www.justin.tv/problems/bml</a>");
+        MotionEvent event = mock(MotionEvent.class);
+        when(event.getAction()).thenReturn(MotionEvent.ACTION_DOWN);
+        when(event.getX()).thenReturn(0f);
+        when(event.getY()).thenReturn(0f);
+        assertTrue(shadowOf(textView).getOnTouchListener().onTouch(textView, event));
+        when(event.getAction()).thenReturn(MotionEvent.ACTION_UP);
+        when(event.getX()).thenReturn(0f);
+        when(event.getY()).thenReturn(0f);
+        assertTrue(shadowOf(textView).getOnTouchListener().onTouch(textView, event));
+        assertNotNull(ShadowApplication.getInstance().getNextStartedActivity());
     }
 }
