4e3ea151d6d6ca6c7bd4a83f5e776114a70dc6f1
Ha Duy Trung <haduytrung@gmail.com>
2015-09-24 12:08:50 +0800

    Make sticky header collapsible

--- app/src/main/java/io/github/hidroh/materialistic/BaseItemActivity.java
+++ app/src/main/java/io/github/hidroh/materialistic/BaseItemActivity.java
@@ -1,25 +1,8 @@
 package io.github.hidroh.materialistic;
 
-import android.os.Bundle;
-import android.support.v7.app.ActionBar;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.ViewGroup;
 
 public abstract class BaseItemActivity extends InjectableActivity {
-
-    protected ViewGroup mContentView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        super.setContentView(R.layout.activity_item_base);
-        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
-        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME |
-                ActionBar.DISPLAY_HOME_AS_UP | ActionBar.DISPLAY_SHOW_TITLE);
-        mContentView = (ViewGroup) findViewById(R.id.content_frame);
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
@@ -28,10 +11,5 @@ public abstract class BaseItemActivity extends InjectableActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void setContentView(int layoutResID) {
-        mContentView.addView(getLayoutInflater().inflate(layoutResID, mContentView, false));
     }
 }
--- app/src/main/java/io/github/hidroh/materialistic/WebActivity.java
+++ app/src/main/java/io/github/hidroh/materialistic/WebActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.ShareActionProvider;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -27,6 +29,10 @@ public class WebActivity extends BaseItemActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME |
+                ActionBar.DISPLAY_HOME_AS_UP | ActionBar.DISPLAY_SHOW_TITLE);
         mItem = getIntent().getParcelableExtra(EXTRA_ITEM);
         if (mItem.getType().equals(ItemManager.Item.COMMENT_TYPE)) {
             setTitle(null);
