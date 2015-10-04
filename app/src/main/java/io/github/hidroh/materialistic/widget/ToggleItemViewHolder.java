28ab2c8affcefb3622f71955cc65ef966cecac67
Ha Duy Trung <haduytrung@gmail.com>
2015-10-04 14:47:18 +0800

    Support color-coded items

--- app/src/main/java/io/github/hidroh/materialistic/widget/ToggleItemViewHolder.java
+++ app/src/main/java/io/github/hidroh/materialistic/widget/ToggleItemViewHolder.java
@@ -7,9 +7,11 @@ import io.github.hidroh.materialistic.R;
 
 public class ToggleItemViewHolder extends ItemRecyclerViewAdapter.ItemViewHolder {
     public final TextView mToggle;
+    public final View mLevel;
 
     public ToggleItemViewHolder(View itemView) {
         super(itemView);
         mToggle = (TextView) itemView.findViewById(R.id.toggle);
+        mLevel = itemView.findViewById(R.id.level);
     }
 }
