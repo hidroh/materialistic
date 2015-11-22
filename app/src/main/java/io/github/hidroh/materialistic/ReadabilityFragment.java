7c99dd02aad42222d597ef5dba707988c2899cb8
Ha Duy Trung <haduytrung@gmail.com>
2015-11-22 21:33:56 +0800

    Add font option for readability

--- app/src/main/java/io/github/hidroh/materialistic/ReadabilityFragment.java
+++ app/src/main/java/io/github/hidroh/materialistic/ReadabilityFragment.java
@@ -26,6 +26,7 @@ public class ReadabilityFragment extends LazyLoadFragment implements Scrollable 
     public static final String EXTRA_URL = ReadabilityFragment.class.getName() + ".EXTRA_URL";
     private static final String STATE_CONTENT = "state:content";
     private static final String STATE_TEXT_SIZE = "state:textSize";
+    private static final String STATE_TYPEFACE_NAME = "state:typefaceName";
     private NestedScrollView mScrollView;
     private TextView mTextView;
     private ProgressBar mProgressBar;
@@ -33,6 +34,8 @@ public class ReadabilityFragment extends LazyLoadFragment implements Scrollable 
     private String mContent;
     private float mTextSize;
     private String[] mTextSizeOptionValues;
+    private String mTypefaceName;
+    private String[] mFontOptionValues;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -41,21 +44,29 @@ public class ReadabilityFragment extends LazyLoadFragment implements Scrollable 
         if (savedInstanceState != null) {
             mTextSize = savedInstanceState.getFloat(STATE_TEXT_SIZE);
             mContent = savedInstanceState.getString(STATE_CONTENT);
+            mTypefaceName = savedInstanceState.getString(STATE_TYPEFACE_NAME);
         } else {
             mTextSize = AppUtils.getDimension(getActivity(),
                     Preferences.Theme.resolvePreferredTextSizeResId(getActivity()),
                     R.attr.contentTextSize);
+            mTypefaceName = Preferences.Theme.getTypeface(getActivity());
         }
         mTextSizeOptionValues = getResources().getStringArray(R.array.pref_text_size_values);
+        mFontOptionValues = getResources().getStringArray(R.array.font_values);
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.menu_font_size, menu);
+        inflater.inflate(R.menu.menu_font_options, menu);
         SubMenu subMenu = menu.findItem(R.id.menu_font_size).getSubMenu();
         String[] options = getResources().getStringArray(R.array.text_size_options);
         for (int i = 0; i < options.length; i++) {
-            subMenu.add(Menu.NONE, Menu.NONE, i, options[i]);
+            subMenu.add(R.id.menu_font_size_group, Menu.NONE, i, options[i]);
+        }
+        subMenu = menu.findItem(R.id.menu_font).getSubMenu();
+        options = getResources().getStringArray(R.array.font_options);
+        for (int i = 0; i < options.length; i++) {
+            subMenu.add(R.id.menu_font_group, Menu.NONE, i, options[i]);
         }
         super.onCreateOptionsMenu(menu, inflater);
     }
@@ -65,10 +76,15 @@ public class ReadabilityFragment extends LazyLoadFragment implements Scrollable 
         if (item.getItemId() == R.id.menu_font_size) {
             return true;
         }
-        mTextSize = AppUtils.getDimension(getActivity(),
-                Preferences.Theme.resolveTextSizeResId(mTextSizeOptionValues[item.getOrder()]),
-                R.attr.contentTextSize);
-        mTextView.setTextSize(TypedValue.COMPLEX_UNIT_PX, mTextSize);
+        if (item.getGroupId() == R.id.menu_font_size_group) {
+            mTextSize = AppUtils.getDimension(getActivity(),
+                    Preferences.Theme.resolveTextSizeResId(mTextSizeOptionValues[item.getOrder()]),
+                    R.attr.contentTextSize);
+            mTextView.setTextSize(TypedValue.COMPLEX_UNIT_PX, mTextSize);
+        } else if (item.getGroupId() == R.id.menu_font_group) {
+            mTypefaceName = mFontOptionValues[item.getOrder()];
+            mTextView.setTypeface(AppUtils.createTypeface(getActivity(), mTypefaceName));
+        }
         return true;
     }
 
@@ -83,6 +99,7 @@ public class ReadabilityFragment extends LazyLoadFragment implements Scrollable 
         mScrollView = (NestedScrollView) view.findViewById(R.id.nested_scroll_view);
         mTextView = (TextView) view.findViewById(R.id.content);
         mTextView.setTextSize(TypedValue.COMPLEX_UNIT_PX, mTextSize);
+        mTextView.setTypeface(AppUtils.createTypeface(getActivity(), mTypefaceName));
         return view;
     }
 
@@ -91,6 +108,7 @@ public class ReadabilityFragment extends LazyLoadFragment implements Scrollable 
         super.onSaveInstanceState(outState);
         outState.putFloat(STATE_TEXT_SIZE, mTextSize);
         outState.putString(STATE_CONTENT, mContent);
+        outState.putString(STATE_TYPEFACE_NAME, mTypefaceName);
     }
 
     @Override
