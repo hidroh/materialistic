1cd24a707fea26ee0f15363dd658d06a54a7b4be
Ha Duy Trung <haduytrung@gmail.com>
2015-11-26 09:01:13 +0800

    Use link movement method for text view links

--- app/src/main/java/io/github/hidroh/materialistic/AppUtils.java
+++ app/src/main/java/io/github/hidroh/materialistic/AppUtils.java
@@ -24,13 +24,9 @@ import android.support.customtabs.CustomTabsIntent;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.IntentCompat;
 import android.text.Html;
-import android.text.Layout;
-import android.text.Spannable;
 import android.text.TextUtils;
 import android.text.format.DateUtils;
-import android.text.style.ClickableSpan;
-import android.view.MotionEvent;
-import android.view.View;
+import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -73,40 +69,7 @@ public class AppUtils {
 
     public static void setTextWithLinks(TextView textView, String htmlText) {
         setHtmlText(textView, htmlText);
-        textView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                int action = event.getAction();
-                if (action == MotionEvent.ACTION_UP ||
-                        action == MotionEvent.ACTION_DOWN) {
-                    int x = (int) event.getX();
-                    int y = (int) event.getY();
-
-                    TextView widget = (TextView) v;
-                    x -= widget.getTotalPaddingLeft();
-                    y -= widget.getTotalPaddingTop();
-
-                    x += widget.getScrollX();
-                    y += widget.getScrollY();
-
-                    Layout layout = widget.getLayout();
-                    int line = layout.getLineForVertical(y);
-                    int off = layout.getOffsetForHorizontal(line, x);
-
-                    ClickableSpan[] link = Spannable.Factory.getInstance()
-                            .newSpannable(widget.getText())
-                            .getSpans(off, off, ClickableSpan.class);
-
-                    if (link.length != 0) {
-                        if (action == MotionEvent.ACTION_UP) {
-                            link[0].onClick(widget);
-                        }
-                        return true;
-                    }
-                }
-                return false;
-            }
-        });
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     public static void setHtmlText(TextView textView, String htmlText) {
