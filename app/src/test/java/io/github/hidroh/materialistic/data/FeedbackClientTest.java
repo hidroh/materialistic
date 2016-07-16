da62aad9f924679e5fff157af4e7c434167be15f
Ha Duy Trung <haduytrung@gmail.com>
2016-07-16 18:53:38 +0800

    Feedback client to use RxJava

--- app/src/test/java/io/github/hidroh/materialistic/data/FeedbackClientTest.java
+++ app/src/test/java/io/github/hidroh/materialistic/data/FeedbackClientTest.java
@@ -3,10 +3,9 @@ package io.github.hidroh.materialistic.data;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.MockitoAnnotations;
 import org.robolectric.RobolectricGradleTestRunner;
+
+import java.io.IOException;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -14,9 +13,7 @@ import javax.inject.Singleton;
 import dagger.Module;
 import dagger.ObjectGraph;
 import dagger.Provides;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import rx.Observable;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
@@ -28,41 +25,36 @@ import static org.mockito.Mockito.when;
 @RunWith(RobolectricGradleTestRunner.class)
 public class FeedbackClientTest {
     @Inject RestServiceFactory factory;
-    @Captor ArgumentCaptor<Callback<Object>> callbackCaptor;
     private FeedbackClient client;
     private FeedbackClient.Callback callback;
-    private Call call;
 
     @Before
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
         ObjectGraph.create(new TestModule()).inject(this);
         reset(TestRestServiceFactory.feedbackService);
         client = new FeedbackClient.Impl(factory);
         callback = mock(FeedbackClient.Callback.class);
-        call = mock(Call.class);
-        when(TestRestServiceFactory.feedbackService
-                .createGithubIssue(any(FeedbackClient.Impl.Issue.class)))
-                .thenReturn(call);
     }
 
     @Test
     public void testSendSuccessful() {
+        when(TestRestServiceFactory.feedbackService
+                .createGithubIssue(any(FeedbackClient.Impl.Issue.class)))
+                .thenReturn(Observable.just(null));
         client.send("title", "body", callback);
         verify(TestRestServiceFactory.feedbackService)
                 .createGithubIssue(any(FeedbackClient.Impl.Issue.class));
-        verify(call).enqueue(callbackCaptor.capture());
-        callbackCaptor.getValue().onResponse(null, Response.success(null));
         verify(callback).onSent(eq(true));
     }
 
     @Test
     public void testSendFailed() {
+        when(TestRestServiceFactory.feedbackService
+                .createGithubIssue(any(FeedbackClient.Impl.Issue.class)))
+                .thenReturn(Observable.error(new IOException()));
         client.send("title", "body", callback);
         verify(TestRestServiceFactory.feedbackService)
                 .createGithubIssue(any(FeedbackClient.Impl.Issue.class));
-        verify(call).enqueue(callbackCaptor.capture());
-        callbackCaptor.getValue().onFailure(null, null);
         verify(callback).onSent(eq(false));
     }
 
