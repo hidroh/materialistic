3c8b2c6ebe7065a5cc51a97a1bb83da57d189935
Johannes Hoffmann <johannes.m.hoffmann@gmx.de>
2019-12-07 17:41:14 +0100

    style: format correctly

--- app/src/main/java/io/github/hidroh/materialistic/AboutActivity.kt
+++ app/src/main/java/io/github/hidroh/materialistic/AboutActivity.kt
@@ -18,45 +18,45 @@ package io.github.hidroh.materialistic
 
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.annotation.IdRes
 import androidx.appcompat.app.ActionBar
-import android.view.MenuItem
 
 class AboutActivity : InjectableActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_about)
-    setSupportActionBar(findViewById(R.id.toolbar))
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
-    supportActionBar!!.displayOptions = ActionBar.DISPLAY_SHOW_HOME or
-        ActionBar.DISPLAY_HOME_AS_UP or ActionBar.DISPLAY_SHOW_TITLE
+        supportActionBar!!.displayOptions = ActionBar.DISPLAY_SHOW_HOME or
+                ActionBar.DISPLAY_HOME_AS_UP or ActionBar.DISPLAY_SHOW_TITLE
 
-    var versionName = ""
-    var versionCode = 0
-    try {
-      versionName = packageManager.getPackageInfo(packageName, 0).versionName
-      versionCode = packageManager.getPackageInfo(packageName, 0).versionCode
-    } catch (e: PackageManager.NameNotFoundException) {
-      // do nothing
+        var versionName = ""
+        var versionCode = 0
+        try {
+            versionName = packageManager.getPackageInfo(packageName, 0).versionName
+            versionCode = packageManager.getPackageInfo(packageName, 0).versionCode
+        } catch (e: PackageManager.NameNotFoundException) {
+            // do nothing
+        }
+
+        setTextWithLinks(R.id.text_application_info, getString(R.string.application_info_text, versionName, versionCode))
+        setTextWithLinks(R.id.text_developer_info, getString(R.string.developer_info_text))
+        setTextWithLinks(R.id.text_libraries, getString(R.string.libraries_text))
+        setTextWithLinks(R.id.text_license, getString(R.string.license_text))
+        setTextWithLinks(R.id.text_3rd_party_licenses, getString(R.string.third_party_licenses_text))
+        setTextWithLinks(R.id.text_privacy_policy, getString(R.string.privacy_policy_text))
     }
 
-    setTextWithLinks(R.id.text_application_info, getString(R.string.application_info_text, versionName, versionCode))
-    setTextWithLinks(R.id.text_developer_info, getString(R.string.developer_info_text))
-    setTextWithLinks(R.id.text_libraries, getString(R.string.libraries_text))
-    setTextWithLinks(R.id.text_license, getString(R.string.license_text))
-    setTextWithLinks(R.id.text_3rd_party_licenses, getString(R.string.third_party_licenses_text))
-    setTextWithLinks(R.id.text_privacy_policy, getString(R.string.privacy_policy_text))
-  }
-
-  private fun setTextWithLinks(@IdRes textViewResId: Int, htmlText: String) {
-    AppUtils.setTextWithLinks(findViewById(textViewResId), AppUtils.fromHtml(htmlText))
-  }
-
-  override fun onOptionsItemSelected(item: MenuItem): Boolean {
-    if (item.itemId == android.R.id.home) {
-      finish()
-      return true
+    private fun setTextWithLinks(@IdRes textViewResId: Int, htmlText: String) {
+        AppUtils.setTextWithLinks(findViewById(textViewResId), AppUtils.fromHtml(htmlText))
     }
-    return super.onOptionsItemSelected(item)
-  }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
