784aadb18c942b12a6825b1cfc95494d6aea1952
Quentin Hibon <qh.public@yahoo.com>
2021-02-19 20:18:54 +0100

    Bump robolectric from 4.2.1 to 4.3.1

--- app/src/test/java/io/github/hidroh/materialistic/WebFragmentTest.kt
+++ app/src/test/java/io/github/hidroh/materialistic/WebFragmentTest.kt
@@ -9,7 +9,6 @@ import android.content.pm.ResolveInfo
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
 import android.net.Uri
-import android.preference.PreferenceManager
 import android.util.Base64
 import android.view.View
 import android.webkit.WebView
@@ -17,14 +16,18 @@ import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.ViewSwitcher
 import androidx.core.widget.NestedScrollView
+import androidx.lifecycle.Lifecycle
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import androidx.preference.PreferenceManager.getDefaultSharedPreferences
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ActivityScenario.launch
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import io.github.hidroh.materialistic.WebFragment.PdfAndroidJavascriptBridge
 import io.github.hidroh.materialistic.data.FavoriteManager
 import io.github.hidroh.materialistic.data.FileDownloader.FileDownloaderCallback
 import io.github.hidroh.materialistic.data.Item
 import io.github.hidroh.materialistic.data.ReadabilityClient
 import io.github.hidroh.materialistic.data.WebItem
-import io.github.hidroh.materialistic.test.TestRunner
 import io.github.hidroh.materialistic.test.WebActivity
 import io.github.hidroh.materialistic.test.shadow.CustomShadows
 import io.github.hidroh.materialistic.test.shadow.ShadowNestedScrollView
@@ -32,7 +35,7 @@ import io.github.hidroh.materialistic.test.shadow.ShadowWebView
 import junit.framework.TestCase.*
 import okio.buffer
 import okio.source
-import org.assertj.android.api.Assertions
+import org.assertj.android.api.Assertions.assertThat
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -41,9 +44,9 @@ import org.mockito.ArgumentMatchers
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
 import org.robolectric.Robolectric
+import org.robolectric.Robolectric.buildActivity
 import org.robolectric.RuntimeEnvironment
-import org.robolectric.Shadows
-import org.robolectric.android.controller.ActivityController
+import org.robolectric.Shadows.shadowOf
 import org.robolectric.annotation.Config
 import org.robolectric.fakes.RoboMenuItem
 import org.robolectric.shadow.api.Shadow
@@ -55,11 +58,10 @@ import java.io.IOException
 import javax.inject.Inject
 
 @Config(shadows = [ShadowWebView::class])
-@RunWith(TestRunner::class)
+@RunWith(AndroidJUnit4::class)
 class WebFragmentTest {
-    private var activity: WebActivity? = null
-    private var controller: ActivityController<WebActivity>? = null
-    private var item: WebItem? = null
+    private lateinit var activity: WebActivity
+    private lateinit var item: WebItem
 
     @JvmField
     @Inject
@@ -68,7 +70,8 @@ class WebFragmentTest {
     @JvmField
     @Inject
     var readabilityClient: ReadabilityClient? = null
-    private var intent: Intent? = null
+
+    private lateinit var scenario: ActivityScenario<WebActivity>
 
     @Before
     fun setUp() {
@@ -78,30 +81,44 @@ class WebFragmentTest {
         item = Mockito.mock(WebItem::class.java)
         Mockito.`when`(item!!.type).thenReturn(Item.STORY_TYPE)
         Mockito.`when`(item!!.url).thenReturn("http://example.com")
-        intent = Intent()
-        intent!!.putExtra(WebActivity.EXTRA_ITEM, item)
-        controller = Robolectric.buildActivity(WebActivity::class.java, intent)
-        Shadows.shadowOf(RuntimeEnvironment.application
+        val intent = Intent().putExtra(WebActivity.EXTRA_ITEM, item)
+
+        // TODO: Frankenstein tests: we shouldn't need this line anymore.
+        buildActivity(WebActivity::class.java, intent)
+
+        scenario = launch(intent)
+        scenario.moveToState(Lifecycle.State.RESUMED)
+        scenario.recreate()
+
+        shadowOf(RuntimeEnvironment.application
                 .getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager)
                 .setActiveNetworkInfo(ShadowNetworkInfo.newInstance(null,
                         ConnectivityManager.TYPE_WIFI, 0, true, NetworkInfo.State.CONNECTED))
-        activity = controller!!.get()
-        PreferenceManager.getDefaultSharedPreferences(activity)
-                .edit()
-                .putBoolean(activity!!.getString(R.string.pref_ad_block), true)
-                .putBoolean(activity!!.getString(R.string.pref_lazy_load), false)
-                .apply()
-        controller!!.create().start().resume().visible()
+
+        scenario.onActivity {
+            it.setVisible(true)
+            getDefaultSharedPreferences(it)
+                    .edit()
+                    .putBoolean(it!!.getString(R.string.pref_ad_block), true)
+                    .putBoolean(it.getString(R.string.pref_lazy_load), false)
+                    .apply()
+        }
+        scenario.onActivity {
+        }
+            scenario.onActivity {
+            activity = it
+        }
     }
+
 
     @Test
     fun testProgressChanged() {
         val progressBar = activity!!.findViewById<ProgressBar>(R.id.progress)
         val webView = activity!!.findViewById<WebView>(R.id.web_view)
-        Shadows.shadowOf(webView).webChromeClient.onProgressChanged(webView, 50)
-        Assertions.assertThat(progressBar).isVisible
-        Shadows.shadowOf(webView).webChromeClient.onProgressChanged(webView, 100)
-        Assertions.assertThat(progressBar).isNotVisible
+        shadowOf(webView).webChromeClient.onProgressChanged(webView, 50)
+        assertThat(progressBar).isVisible
+        shadowOf(webView).webChromeClient.onProgressChanged(webView, 100)
+        assertThat(progressBar).isNotVisible
     }
 
     @Test
@@ -111,16 +128,16 @@ class WebFragmentTest {
         resolverInfo.activityInfo.applicationInfo = ApplicationInfo()
         resolverInfo.activityInfo.applicationInfo.packageName = ListActivity::class.java.getPackage().name
         resolverInfo.activityInfo.name = ListActivity::class.java.name
-        val rpm = Shadows.shadowOf(RuntimeEnvironment.application.packageManager)
+        val rpm = shadowOf(RuntimeEnvironment.application.packageManager)
         val url = "http://example.com/file.doc"
         rpm.addResolveInfoForIntent(Intent(Intent.ACTION_VIEW, Uri.parse(url)), resolverInfo)
         val webView = activity!!.findViewById<WebView>(R.id.web_view)
         val shadowWebView = Shadow.extract<ShadowWebView>(webView)
         Mockito.`when`(item!!.url).thenReturn(url)
         shadowWebView.downloadListener.onDownloadStart(url, "", "", "", 0L)
-        Assertions.assertThat(activity!!.findViewById(R.id.empty) as View).isVisible
+        assertThat(activity!!.findViewById(R.id.empty) as View).isVisible
         activity!!.findViewById<View>(R.id.download_button).performClick()
-        assertNotNull(Shadows.shadowOf(activity).nextStartedActivity)
+        assertNotNull(shadowOf(activity).nextStartedActivity)
     }
 
     @Test
@@ -130,7 +147,7 @@ class WebFragmentTest {
         resolverInfo.activityInfo.applicationInfo = ApplicationInfo()
         resolverInfo.activityInfo.applicationInfo.packageName = ListActivity::class.java.getPackage().name
         resolverInfo.activityInfo.name = ListActivity::class.java.name
-        val rpm = Shadows.shadowOf(RuntimeEnvironment.application.packageManager)
+        val rpm = shadowOf(RuntimeEnvironment.application.packageManager)
         Mockito.`when`(item!!.url).thenReturn("http://example.com/file.pdf")
         rpm.addResolveInfoForIntent(Intent(Intent.ACTION_VIEW, Uri.parse(item!!.url)), resolverInfo)
         val webView = activity!!.findViewById<WebView>(R.id.web_view)
@@ -183,21 +200,29 @@ class WebFragmentTest {
 
     @Test
     fun testFullscreen() {
-        LocalBroadcastManager.getInstance(activity!!)
-                .sendBroadcast(Intent(WebFragment.ACTION_FULLSCREEN)
-                        .putExtra(WebFragment.EXTRA_FULLSCREEN, true))
-        Assertions.assertThat(activity!!.findViewById(R.id.control_switcher) as View).isVisible
-        activity!!.recreate()
-        Assertions.assertThat(activity!!.findViewById(R.id.control_switcher) as View).isVisible
-        activity!!.findViewById<View>(R.id.button_exit).performClick()
-        Assertions.assertThat(activity!!.findViewById(R.id.control_switcher) as View).isNotVisible
+        scenario.onActivity {
+            LocalBroadcastManager.getInstance(it)
+                    .sendBroadcast(Intent(WebFragment.ACTION_FULLSCREEN)
+                            .putExtra(WebFragment.EXTRA_FULLSCREEN, true))
+            assertThat(it.findViewById<View?>(R.id.control_switcher)).isVisible
+        }
+
+        scenario.recreate()
+
+        scenario.onActivity {
+            assertThat(it!!.findViewById(R.id.control_switcher) as View).isVisible
+            it.findViewById<View>(R.id.button_exit).performClick()
+            assertThat(it.findViewById(R.id.control_switcher) as View).isNotVisible
+        }
     }
 
     @Test
     fun testSearch() {
-        LocalBroadcastManager.getInstance(activity!!)
-                .sendBroadcast(Intent(WebFragment.ACTION_FULLSCREEN)
-                        .putExtra(WebFragment.EXTRA_FULLSCREEN, true))
+        activity!!.sendBroadcast(Intent(WebFragment.ACTION_FULLSCREEN)
+                .putExtra(WebFragment.EXTRA_FULLSCREEN, true))
+//        LocalBroadcastManager.getInstance(activity!!)
+//                .sendBroadcast(Intent(WebFragment.ACTION_FULLSCREEN)
+//                        .putExtra(WebFragment.EXTRA_FULLSCREEN, true))
         activity!!.findViewById<View>(R.id.button_find).performClick()
         val controlSwitcher = activity!!.findViewById<ViewSwitcher>(R.id.control_switcher)
         org.assertj.core.api.Assertions.assertThat(controlSwitcher.displayedChild).isEqualTo(1)
@@ -205,33 +230,32 @@ class WebFragmentTest {
 
         // no query
         val editText = activity!!.findViewById<EditText>(R.id.edittext)
-        Shadows.shadowOf(editText).onEditorActionListener.onEditorAction(null, 0, null)
-        Assertions.assertThat(activity!!.findViewById(R.id.button_next) as View).isDisabled
+        shadowOf(editText).onEditorActionListener.onEditorAction(null, 0, null)
+        assertThat(activity!!.findViewById(R.id.button_next) as View).isDisabled
 
         // with results
         shadowWebView.setFindCount(1)
         editText.setText("abc")
-        Shadows.shadowOf(editText).onEditorActionListener.onEditorAction(null, 0, null)
-        Assertions.assertThat(activity!!.findViewById(R.id.button_next) as View).isEnabled
+        shadowOf(editText).onEditorActionListener.onEditorAction(null, 0, null)
+        assertThat(activity!!.findViewById(R.id.button_next) as View).isEnabled
         activity!!.findViewById<View>(R.id.button_next).performClick()
         org.assertj.core.api.Assertions.assertThat(shadowWebView.findIndex).isEqualTo(1)
         activity!!.findViewById<View>(R.id.button_clear).performClick()
-        Assertions.assertThat(editText).isEmpty
+        assertThat(editText).isEmpty
         org.assertj.core.api.Assertions.assertThat(controlSwitcher.displayedChild).isEqualTo(0)
 
         // with no results
         shadowWebView.setFindCount(0)
         editText.setText("abc")
-        Shadows.shadowOf(editText).onEditorActionListener.onEditorAction(null, 0, null)
-        Assertions.assertThat(activity!!.findViewById(R.id.button_next) as View).isDisabled
+        shadowOf(editText).onEditorActionListener.onEditorAction(null, 0, null)
+        assertThat(activity!!.findViewById(R.id.button_next) as View).isDisabled
         org.assertj.core.api.Assertions.assertThat(ShadowToast.getTextOfLatestToast()).contains(activity!!.getString(R.string.no_matches))
     }
 
     @Test
     fun testRefresh() {
-        LocalBroadcastManager.getInstance(activity!!)
-                .sendBroadcast(Intent(WebFragment.ACTION_FULLSCREEN)
-                        .putExtra(WebFragment.EXTRA_FULLSCREEN, true))
+        activity!!.sendBroadcast(Intent(WebFragment.ACTION_FULLSCREEN)
+                .putExtra(WebFragment.EXTRA_FULLSCREEN, true))
         ShadowWebView.lastGlobalLoadedUrl = null
         val shadowWebView = Shadow.extract<ShadowWebView>(activity!!.findViewById(R.id.web_view))
         shadowWebView.progress = 20
@@ -245,16 +269,15 @@ class WebFragmentTest {
     @SuppressLint("NewApi")
     @Test
     fun testWebControls() {
-        LocalBroadcastManager.getInstance(activity!!)
-                .sendBroadcast(Intent(WebFragment.ACTION_FULLSCREEN)
-                        .putExtra(WebFragment.EXTRA_FULLSCREEN, true))
+        activity!!.sendBroadcast(Intent(WebFragment.ACTION_FULLSCREEN)
+                .putExtra(WebFragment.EXTRA_FULLSCREEN, true))
         val shadowWebView = Shadow.extract<ShadowWebView>(activity!!.findViewById(R.id.web_view))
         activity!!.findViewById<View>(R.id.button_more).performClick()
-        Shadows.shadowOf(ShadowPopupMenu.getLatestPopupMenu()).onMenuItemClickListener
+        shadowOf(ShadowPopupMenu.getLatestPopupMenu()).onMenuItemClickListener
                 .onMenuItemClick(RoboMenuItem(R.id.menu_zoom_in))
         org.assertj.core.api.Assertions.assertThat(shadowWebView.zoomDegree).isEqualTo(1)
         activity!!.findViewById<View>(R.id.button_more).performClick()
-        Shadows.shadowOf(ShadowPopupMenu.getLatestPopupMenu()).onMenuItemClickListener
+        shadowOf(ShadowPopupMenu.getLatestPopupMenu()).onMenuItemClickListener
                 .onMenuItemClick(RoboMenuItem(R.id.menu_zoom_out))
         org.assertj.core.api.Assertions.assertThat(shadowWebView.zoomDegree).isEqualTo(0)
         activity!!.findViewById<View>(R.id.button_forward).performClick()
@@ -296,10 +319,10 @@ class WebFragmentTest {
     @Test
     fun testBackPressed() {
         val webView = activity!!.findViewById<WebView>(R.id.web_view)
-        Shadows.shadowOf(webView).webViewClient.onPageFinished(webView, "http://example.com")
-        Shadows.shadowOf(webView).setCanGoBack(true)
+        shadowOf(webView).webViewClient.onPageFinished(webView, "http://example.com")
+        shadowOf(webView).setCanGoBack(true)
         assertTrue(activity!!.fragment.onBackPressed())
-        Shadows.shadowOf(webView).setCanGoBack(false)
+        shadowOf(webView).setCanGoBack(false)
         assertFalse(activity!!.fragment.onBackPressed())
     }
 
@@ -311,6 +334,6 @@ class WebFragmentTest {
 
     @After
     fun tearDown() {
-        controller!!.pause().stop().destroy()
+        scenario.close()
     }
 }
