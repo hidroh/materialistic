7a4aac72c1199b461c914cc3490847003a9ce366
Ha Duy Trung <haduytrung@gmail.com>
2021-08-02 17:48:48 +0700

    Fix coveralls JDK11

--- .travis/coverage.sh
+++ .travis/coverage.sh
@@ -2,4 +2,5 @@
 set -v
 if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TASK}" = "test" ]; then
     ./gradlew -Pci --console=plain jacocoTestReport coveralls
+    curl -F 'json_file=@build/coveralls/report.json' 'https://coveralls.io/api/v1/jobs'
 fi
